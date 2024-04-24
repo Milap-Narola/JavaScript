4612d45bcf9d21ecd173371f49fa5f708feaff0c
Milap Narola <milap5322@gmail.com>
2024-04-24 18:36:55 +0530

    ADD

---
+++ Lecture-3/Q_4.js
@@ -0,0 +1,14 @@
+/*Q.4 create a variable give a name, number then assign a value then after you have the check and print it ,
+if the value is odd print it's an odd number else it's an even number
+input
+4
+output 
+it's an even number */
+let n=4 ;
+
+if (n%2==0) {
+    console.log("It's An Even Number",n);
+}
+else{
+    console.log("It's An Odd Number",n);
+}
