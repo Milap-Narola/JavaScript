bcf346dc17588916e85d995fd150b352fd8fb3b0
Milap Narola <milap5322@gmail.com>
2024-04-24 16:32:22 +0530

    ADD

---
+++ Lecture-2/Q_3.js
@@ -0,0 +1,13 @@
+/*Q.3 you have to create  five variables, stored in the variables with the following names,
+one, two, three, four, five
+and assign value to all
+then after you have to sum all and make it double 
+suppose after sum you got 50 
+the result should be 100  */
+
+let a = 10;
+let b = 15;
+let c = 5;
+let d = 8;
+let e = 12;
+console.log((a + b + c + d + e) * 2);
---
+++ Lecture-2/Q_4.js
@@ -0,0 +1,12 @@
+/*Q.4  you have to create 7 variables like A, B, C, D, E, F, G. Find out the product of (A + B + C) * (D + E + F + G). */
+
+
+
+let a = 10
+let b = 20
+let c = 30
+let d = 40
+let e = 50
+let f = 60
+let g = 70
+console.log((a + b + c) * (d + c + e + f + g));
