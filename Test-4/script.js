427f775feb5015e53d5d86f6b76aed5a4f366a27
Milap Narola <milap5322@gmail.com>
2024-06-03 11:38:18 +0530

    Add

--- Test-4/script.js
+++ Test-4/script.js
@@ -1,33 +1,37 @@
-let Bank = []
-document.getElementById("contact").addEventListener("submit", Bankaccount = (e) => {
+let Bank = [];
+document.getElementById("contact").addEventListener("submit", (e) => {
     e.preventDefault();
+
     class Bankaccount {
         constructor() {
-            this.userName = getElementById("Username").value;
-                this.AccountNumber = getElementById("acnumber").value;
-                this.Amount = getElementById("Amount").value;
+            this.userName = document.getElementById("Username").value;
+            this.AccountNumber = document.getElementById("acnumber").value;
+            this.Amount = document.getElementById("Amount").value;
         }
+
         getDetails() {
-            return this.userName, this.AccountNumber, this.Amount;
+            return {
+                userName: this.userName,
+                AccountNumber: this.AccountNumber,
+                Amount: this.Amount
+            };
         }
     }
 
-    let Account = new Bankaccount();
-    Account.push(Account);
+    let account = new Bankaccount();
+    Bank.push(account);
     console.log(Bank);
 
-    document.getElementById("Tbody").innerHTML = ""
-    Account.map((ele) => {
+    document.getElementById("Tbody").innerHTML = "";
+    Bank.forEach((ele) => {
         let tr = document.createElement("tr");
         let td1 = document.createElement("td");
         td1.innerHTML = ele.userName;
         let td2 = document.createElement("td");
         td2.innerHTML = ele.AccountNumber;
-        let td4 = document.createElement("td");
-        td4.innerHTML = ele.Amount;
-        tr.append(td1, td2, td4);
+        let td3 = document.createElement("td");
+        td3.innerHTML = ele.Amount;
+        tr.append(td1, td2, td3);
         document.getElementById("Tbody").append(tr);
-
-
-    })
-})
+    });
+});
