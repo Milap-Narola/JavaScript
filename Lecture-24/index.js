2dea1cbeb71289593e80e36f7ca1bc0af4aa8c99
Milap Narola <milap5322@gmail.com>
2024-07-17 17:45:57 +0530

    Add

--- Lecture-24/index.js
+++ Lecture-24/index.js
@@ -1,15 +1,17 @@
 
-const scriptURL='https://script.google.com/macros/s/AKfycbw-EwVXVzTqTms-DNYvTpdrAGrG-t0g8UunIPyualm8Ty0yZjFRLYiR-fRCwL-7qwG9AQ/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw-EwVXVzTqTms-DNYvTpdrAGrG-t0g8UunIPyualm8Ty0yZjFRLYiR-fRCwL-7qwG9AQ/exec';
 
-document.getElementById('FormData').addEventListener('submit',(e)=>{
-e.preventDefault()
-fetch(scriptURL, {method:'POST',body: new FormData(form)})
-.then(response=>alert('Your form has been submitted successfully '+response + '.')) 
-.then(()=>{window. location.reload();})
-.catch(error=>console.error('Error !', error))
-
-
-})
+document.getElementById('FormData').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = document.getElementById('FormData');
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => response.text())  // convert response to text
+        .then(responseText => {
+            alert('Your form has been submitted successfully: ' + responseText);
+            window.location.reload();
+        })
+        .catch(error => console.error('Error!', error));
+});
 
 
 
